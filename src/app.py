9a5f4ff237780e6e6e7a46f972b3d2af0bf8279e
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2025-04-23 12:29:56 +0000

    Adiciona validação para verificar se o aluno já está inscrito em uma atividade

--- src/app.py
+++ src/app.py
@@ -87,7 +87,7 @@ def root():
 def get_activities():
     return activities
 
-
+# Validar se o aluno já está inscrito
 @app.post("/activities/{activity_name}/signup")
 def signup_for_activity(activity_name: str, email: str):
     """Sign up a student for an activity"""
